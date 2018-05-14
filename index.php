2d348957f19b7cac120a5de3806c873256551f23
lbertelli <lbertelli29@gmail.com>
2018-05-14 12:05:17 -0300

    nova aula
    novo estudo.

--- index.php
+++ index.php
@@ -18,7 +18,28 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
 		<script>
-			// código javascript
+			$(document).ready(function(){
+				//verificar se os campos foram devidamente preenchidos
+				$('#btn_login').click(function(){
+
+					var campo_vazio = false;
+
+					if($('#campo_usuario').val() == ''){
+						$('#campo_usuario').css({'border-color': '#A94442'});
+						var campo_vazio = true;
+					}else {
+						$('#campo_usuario').css({'border-color': '#CCC'});
+					}
+					if($('#campo_senha').val() == ''){
+						$('#campo_senha').css({'border-color': '#A94442'});
+						var campo_vazio = true;
+					} else {
+						$('#campo_senha').css({'border-color': '#CCC'});
+					}
+					if (campo_vazio) return false;
+
+				});
+			});
 		</script>
 	</head>
 
--- validar_acesso.php
+++ validar_acesso.php
@@ -1,11 +1,13 @@
 <?php
 
+    session_start();
+
     require_once('db.class.php');
 
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
-    $sql = " SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
+    $sql = " SELECT usuario,email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
@@ -15,14 +17,16 @@
     if ($resultado_id) {
       $dados_usuario = mysqli_fetch_array($resultado_id);
 
-      if (isset($dados_usuario['usuario'])) {
-        echo 'usuário existe';
-      }
+      if (isset($dados_usuario['usuario'])){
 
-      else {
+        $_SESSION['usuario'] = $dados_usuario['usuario'];
+        $_SESSION['email'] = $dados_usuario['email'];
+
+        header('Location: home.php');
+
+      } else {
         header('Location: index.php?erro=1');
       }
-
     }else {
       echo 'Erro na execução da consulta, favor entrar em contato com o admin do site';
     }
